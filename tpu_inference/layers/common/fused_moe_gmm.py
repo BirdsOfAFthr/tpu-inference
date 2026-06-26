94dc96e3c96b92257ed61f1f52f2d3f8bfef4069
Amanda Liang <amandaliang@google.com>
2026-06-26 18:16:04 +0000

    Support compressed-tensors quantization, optimize cluster serving launch, and update jax/libtpu for dense gather reduce
    Signed-off-by: Amanda Liang <amandaliang@google.com>

--- tpu_inference/layers/common/fused_moe_gmm.py
+++ tpu_inference/layers/common/fused_moe_gmm.py
@@ -23,7 +23,10 @@ from jax.sharding import PartitionSpec as P
 import tpu_inference.envs as envs
 from tpu_inference.kernels.collectives import \
     hierarchical_reduce_scatter as hier_rs
-from tpu_inference.kernels.megablox.gmm_v2 import gmm_v2
+from tpu_inference.kernels.megablox.gmm_v2 import (TileSizes, calculate_tiling,
+                                                   gmm_v2)
+from tpu_inference.kernels.sparse_core.dense_gather_reduce import \
+    dense_gather_reduce
 from tpu_inference.kernels.sparse_core.ragged_gather import \
     ragged_gather as ragged_gather_v1
 from tpu_inference.kernels.sparse_core.ragged_gather_reduce import \
@@ -125,7 +128,8 @@ def gmm_wrapper(lhs,
                 group_sizes,
                 group_offset,
                 fuse_act=None,
-                preferred_element_type=None):
+                preferred_element_type=None,
+                tile_info=calculate_tiling):
     gmm_res = gmm_v2(
         lhs=lhs,
         rhs=rhs,
@@ -136,6 +140,7 @@ def gmm_wrapper(lhs,
         zero_initialize=False,
         fuse_act=fuse_act,
         preferred_element_type=preferred_element_type,
+        tile_info=tile_info,
     )
     return gmm_res
 
@@ -195,13 +200,28 @@ def moe_gmm_local(x: jax.Array,
                   enable_rs_kernel: bool = False,
                   onehot_moe_permute_threshold: int = 0,
                   scatter_results: bool = False,
-                  moe_chunk_size: int = 0) -> jax.Array:
+                  moe_chunk_size: int = 0,
+                  use_ep: bool = False) -> jax.Array:
     """Main MoE logic on a local shard can run in TP or EP mode.
 
     Set parallelism for "tp" or "ep"
     """
 
     assert parallelism in ["tp", "ep"]
+
+    # Define your custom hardcoded tiling configs directly here (e.g., TileSizes(tile_m=128, tile_k=128, tile_n=128))
+    m = x.shape[0]
+    is_decode = (m <= 512)
+    if is_decode or use_ep:
+        # decode tiling config
+        gmm1_tiling = calculate_tiling
+        gmm2_tiling = calculate_tiling
+    else:
+        # prefill tiling config
+        gmm1_tiling = TileSizes(tile_m=512, tile_k=7168,
+                                tile_n=256)  # xid/259725198
+        gmm2_tiling = TileSizes(tile_m=512, tile_k=256,
+                                tile_n=7168)  # xid/259805054
 
     # GMM1 computes x @ (W_up | W_gate) together and activation, output is [tokens,padded_intermediate_size]
     gmm1_res = gmm_wrapper(
@@ -213,6 +233,7 @@ def moe_gmm_local(x: jax.Array,
         group_offset,
         fuse_act=activation,
         preferred_element_type=x.dtype,
+        tile_info=gmm1_tiling,
     )
 
     # When the parallelism is TP since w2_bias is not sharded, we should only apply bias
@@ -222,8 +243,13 @@ def moe_gmm_local(x: jax.Array,
         shard_id = jax.lax.axis_index(ShardingAxisName.MLP_TENSOR).sum()
         w2_bias = jnp.where(shard_id == 0, w2_bias, 0)
     gmm1_res = gmm1_res[:, :w2.shape[1]]  # trim to hidden size if padded
-    gmm2_res = gmm_wrapper(gmm1_res, w2, w2_scale, w2_bias, group_sizes,
-                           group_offset)
+    gmm2_res = gmm_wrapper(gmm1_res,
+                           w2,
+                           w2_scale,
+                           w2_bias,
+                           group_sizes,
+                           group_offset,
+                           tile_info=gmm2_tiling)
 
     batch_size = gmm2_res.shape[0]
     local_group_size = w1.shape[0]
@@ -314,13 +340,12 @@ def moe_gmm_local(x: jax.Array,
                                                     cur_weights.reshape(-1),
                                                     cur_mask.reshape(-1), topk)
         else:
-            token_hidden_full = gmm2_res[cur_indices]
-            cur_sorted = token_hidden_full.reshape(
-                (-1, topk, gmm2_res.shape[-1]))
-            cur_topk_weights = jnp.expand_dims(cur_weights, axis=-1)
-            cur_weighted = cur_sorted * cur_topk_weights
-            cur_masked = jnp.where(cur_mask, cur_weighted, 0.0)
-            chunk_hidden = cur_masked.sum(axis=-2)
+            chunk_hidden = dense_gather_reduce(
+                gmm2_res,
+                topk_argsort_revert_indices,
+                topk_weights,
+                topk,
+            )
 
         if enable_rs_kernel:
             # Fallback to psum-scatter for small token sizes to avoid Mosaic compilation.
@@ -500,6 +525,7 @@ def expert_parallel_gmm(
             enable_rs_kernel=enable_rs_kernel,
             scatter_results=scatter_results,
             moe_chunk_size=moe_chunk_size,
+            use_ep=True,
         ),
         mesh=mesh,
         in_specs=(
